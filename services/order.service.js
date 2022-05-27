50242d76ee3b8d22b7d519f61fd471e6d65b05a3
JairoDuenas <jairoduenas.ing@gmail.com>
2022-05-27 08:10:08 -0500

    Ordenes de compra

--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,36 @@
 const boom = require('@hapi/boom');
-
+const{models} = require('./../libs/sequelize');
 class OrderService {
-
-  constructor(){
+  constructor() {
   }
+
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data);
+    return newOrder;
   }
 
   async find() {
-    return [];
+    return[];
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id, {
+      include: [
+        {
+        association:'customer',
+        include:['user']
+        }
+      ]
+    });
+    return order;
   }
-
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+  async update(id,changes) {
+    return{id,changes,};
   }
 
   async delete(id) {
-    return { id };
+    return{id};
   }
-
 }
 
 module.exports = OrderService;
