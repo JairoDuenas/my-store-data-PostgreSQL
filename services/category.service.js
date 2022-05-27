2e11386104fed963372a06d93947ed3b97ac6a4d
JairoDuenas <jairoduenas.ing@gmail.com>
2022-05-26 20:39:23 -0500

    Resolciendo relaciones uno a muchos

--- services/category.service.js
+++ services/category.service.js
@@ -1,62 +1,34 @@
 const boom = require('@hapi/boom');
-const pool = require('../libs/postgres.pool');
-
+const { models }= require('./../libs/sequelize');
 class CategoryService {
 
   constructor(){
-    this.pool = pool;
-    this.pool.on("error", err => console.error(err));
+  }
+  async create(data) {
+    const newCategory = await models.Category.create(data);
+    return newCategory;
   }
 
   async find() {
-    const query = "SELECT * FROM CATEGORIES";
-    const categories = await this.pool.query(query);
-    return categories.rows;
+    const categories = await models.Category.findAll();
+    return categories;
   }
 
   async findOne(id) {
-    const query = "SELECT * FROM CATEGORIES WHERE ID = $1";
-    const category = await this.pool.query(query, [id]);
-    return category.rows;
-  }
-
-  async create(data) {
-    let { name, items } = data;
-    const queryId = "SELECT (MAX(ID) + 1) AS ID FROM CATEGORIES";
-    const { rows } = await this.pool.query(queryId);
-
-    if (!items) {
-      items = 0;
-    }
-    const values = [rows[0].id, name, items];
-    const query = "INSERT INTO CATEGORIES (ID, NAME, ITEMS) VALUES ($1, $2, $3)"
-    await this.pool.query(query, values);
-
-    return {
-      id: rows[0].id,
-      ...data
-    };
+    const category = await models.Category.findByPk(id, {
+      include: ['products']
+    });
+    return category;
   }
 
   async update(id, changes) {
-    const dataUpdate = [];
-    const setQuery = [];
-
-    Object.entries(changes).forEach((entrie, index) => {
-      setQuery.push(entrie[0] + `= $${index + 1}`);
-      dataUpdate.push(entrie[1]);
-    });
-    const query = `UPDATE CATEGORIES SET ${setQuery.join(", ")} WHERE ID = ${id}`;
-    await this.pool.query(query, dataUpdate);
     return {
       id,
-      ...changes,
+      changes,
     };
   }
 
   async delete(id) {
-    const query = "SELECT * FROM CATEGORIES WHERE ID = $1";
-    await this.pool.query(query, [id]);
     return { id };
   }
 
